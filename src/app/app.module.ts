efd76affb7cda612386a42529cb99ea5c7862956
Clément Deriencourt <clement-deriencourt@orange.fr>
2020-09-24 14:55:00 +0200

    Table w/ select 1by1 or all w/ case ( box )

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableSelectComponent } from './table-select/table-select.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatSelectModule} from '@angular/material/select';
+import {MatCheckbox, MatCheckboxModule} from '@angular/material/checkbox';
+import {CdkTableModule} from '@angular/cdk/table';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableSelectComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
+    MatTableModule,
+    CdkTableModule,
+    MatCheckboxModule,
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA,
   ],
   providers: [],
   bootstrap: [AppComponent]
